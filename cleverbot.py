2c9c19ce8f635ca27b3e9f05e0b7ba24b80d3b17
Hendrixthecool <HendrixBryon@gitmail.com>
2014-05-07 16:03:22 -0700

    pythonroom update

--- cleverbot.py
+++ cleverbot.py
@@ -1,4 +1,5 @@
 # author: Hendrixthecool
 from random import randint
 age = randint(0, 102)
-print str (age) + ". is that your age?"
+name = input("what is your name")
+print str (age) + ". is that your age? " + (name)
