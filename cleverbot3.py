c33c05f67c68761503e8f7bfbe1d8836d2c78264
Hendrixthecool <HendrixBryon@gitmail.com>
2014-05-07 16:27:00 -0700

    pythonroom update

--- cleverbot3.py
+++ cleverbot3.py
@@ -3,8 +3,8 @@ answer = input("Speak human.")
 answer = answer.lower()
 two = answer.split()
 battery = 100
-while battery < 100:
-	while len(two) > 1:
+while battery > 0:
+	while len(two) > 0:
 		if two[0] == "are":
 			if two[1] == "you":
 				if two[2] == "crazy!?!?":
@@ -17,4 +17,4 @@ while battery < 100:
 		print("I cant here you")
 		battery = battery - 1
 		print("so -1 battery and now i have"),
-		print str(battery) + "% battery left")
+		print str (battery) + "% battery left"
