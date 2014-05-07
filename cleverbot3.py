c71d0953a9565fc2743258ba85733ec50694758d
Hendrixthecool <HendrixBryon@gitmail.com>
2014-05-07 16:25:01 -0700

    pythonroom update

--- cleverbot3.py
+++ cleverbot3.py
@@ -1,3 +1,20 @@
 # author: Hendrixthecool
 answer = input("Speak human.")
-print(answer)
+answer = answer.lower()
+two = answer.split()
+battery = 100
+while battery < 100:
+	while len(two) > 1:
+		if two[0] == "are":
+			if two[1] == "you":
+				if two[2] == "crazy!?!?":
+					print("no, i am CLEVER")
+			if two[1] == "you":
+				if two[2] == "a":
+					if two[3] == "superhero?":
+						print "yes"
+	else:
+		print("I cant here you")
+		battery = battery - 1
+		print("so -1 battery and now i have"),
+		print str(battery) + "% battery left")
