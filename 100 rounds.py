97c0cfc0b17ef38bae3caeb9d74d2e3e243b03ee
Hendrixthecool <HendrixBryon@gitmail.com>
2014-05-07 15:40:25 -0700

    pythonroom update

--- 100 rounds.py
+++ 100 rounds.py
@@ -1,5 +1,13 @@
 # author: Hendrixthecool
-switch = 0 
+switch = 
 Round = 0
 people = range(1, 101)
-while
+while Round < 100:
+	if people % Round == 0:
+		if switch == True:
+			switch = False
+			switch = switch + 1
+		if switch == False:
+			switch = True
+			switch = switch + 1
+print str (switch)
